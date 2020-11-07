7e6bf79d683475fb5d3f13ed44c39ae6d8bc1802
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-11-07 16:56:28 +0100

    Primer commit

---
+++ EditorTexto/src/editortexto/EditorTexto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editortexto;
+
+
+
+/**
+ *
+ * @author Javier de la Llave
+ * 
+ * Clase que abre la única ventana de la aplicación
+ */
+public class EditorTexto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Para que se vea la interfaz al abrir la aplicación
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+
+        
+        
+    }
+
+
+
+}
